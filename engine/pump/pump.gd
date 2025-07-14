314df09e8a79afa54ec6b16e7a6d38e2b8bcc069
Peter Askey <peter_g_askey@live.co.uk>
2025-07-14 13:15:28 +0200

    fixed pump minigame

--- engine/pump/pump.gd
+++ engine/pump/pump.gd
@@ -1,7 +1,6 @@
 class_name Pump extends Node2D
 
-#@onready var _gauge : Sprite2D = %Gauge as Sprite2D
-
+@onready var gauge : Sprite2D = %Gauge as Sprite2D
 @onready var button_sprite : Sprite2D = %ButtonSprite
 
 var button_pressed : Texture = preload("res://engine/pump/pump_button_down.png")
@@ -10,11 +9,31 @@ var button_released : Texture = preload("res://engine/pump/pump_button_up.png")
 var empty_rotation_degrees := -125.0
 var full_rotation_degrees := 125.0
 
+var tween : Tween  = null
+
 signal puzzle_completed()
 
+
+func _process(delta: float) -> void:
+	if gauge.rotation_degrees < full_rotation_degrees:
+		gauge.rotation_degrees = move_toward(gauge.rotation_degrees, -125, delta * 20)	
 
 func _on_button_input_event(_viewport: Node, _event: InputEvent, _shape_idx: int) -> void:
 	if Input.is_action_just_pressed("lmb"):
 		button_sprite.texture = button_pressed
+		
+		var random_degree := randi_range(15,30)
+		
+		if gauge.rotation_degrees < full_rotation_degrees:
+			if tween != null:
+				tween.stop()
+			tween = create_tween()
+			var new_val := mini(gauge.rotation_degrees + random_degree, full_rotation_degrees)
+			tween.tween_property(gauge, "rotation_degrees", new_val, 0.05).set_trans(Tween.TRANS_BOUNCE)
+			#gauge.rotation_degrees = mini(gauge.rotation_degrees + random_degree, full_rotation_degrees)
+		
+		if gauge.rotation_degrees >= full_rotation_degrees:
+			puzzle_completed.emit()
+		
 	elif  Input.is_action_just_released("lmb"):
 		button_sprite.texture = button_released
